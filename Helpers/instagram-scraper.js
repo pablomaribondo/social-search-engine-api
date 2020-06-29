51f5f477fcb9335010cbc9a5822ed7613b2bec71
Pablo Maribondo <pablomgvieira@gmail.com>
2020-06-28 21:23:07 -0300

    make it better

--- Helpers/instagram-scraper.js
+++ Helpers/instagram-scraper.js
@@ -12,8 +12,7 @@ module.exports.getProfiles = async (request, links) => {
 
     const scrape = () => {
       return new Promise((resolve) => {
-        curl.on('end', (statusCode, data, headers) => {
-          console.log(headers);
+        curl.on('end', (statusCode, data) => {
           const match = data.match(
             /<script type="text\/javascript">window\._sharedData\s?=(.+);<\/script>/
           );
